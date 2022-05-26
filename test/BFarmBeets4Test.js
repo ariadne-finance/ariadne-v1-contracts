393f6b6433f86a9b1bbf357d20cecf1de0f0f9ef
Egor Egorov <me@egorfine.com>
2022-05-26 18:48:39 +0300

    abi moved inside into the tests

--- test/BFarmBeets4Test.js
+++ test/BFarmBeets4Test.js
@@ -1,5 +1,4 @@
 const chai = require('chai');
-const fs = require('fs');
 const { solidity } = require('ethereum-waffle');
 const { locatePoolIdFromMasterChef } = require('../utils/BFarmUtils.js');
 
@@ -7,10 +6,6 @@ chai.use(solidity);
 const expect = chai.expect;
 
 const poolAddress = '0xf3A602d30dcB723A74a0198313a7551FEacA7DAc';
-
-function readAbi(name) {
-  return JSON.parse(fs.readFileSync(__dirname + '/../../' + name + '.json').toString());
-}
 
 describe('BFarmBeets4', function () {
   let bFarmBeets;
@@ -21,7 +16,7 @@ describe('BFarmBeets4', function () {
     let deployerAccount;
     [ deployerAccount, traderAccount, managerAccount ] = await hre.ethers.getSigners();
 
-    masterChef = new ethers.Contract('0x8166994d9ebBe5829EC86Bd81258149B87faCfd3', readAbi('abi-additional/beets/BeethovenxMasterChef'), ethers.provider);
+    masterChef = new ethers.Contract('0x8166994d9ebBe5829EC86Bd81258149B87faCfd3', BeetsBeethovenxMasterChefAbi, ethers.provider);
 
     masterChefPoolId = await locatePoolIdFromMasterChef(masterChef, poolAddress);
     if (masterChefPoolId === null) {
@@ -48,11 +43,10 @@ describe('BFarmBeets4', function () {
 
     await bFarmBeets.grantRole(MANAGER_ROLE, managerAccount.address);
     await bFarmBeets.grantRole(TRADER_ROLE, traderAccount.address);
+    pool = new ethers.Contract(poolAddress, BeetsWeightedPoolAbi, ethers.provider);
 
-    pool = new ethers.Contract(poolAddress, readAbi('abi-additional/beets/WeightedPool'), ethers.provider);
-
-    const vault = new ethers.Contract(await pool.getVault(), readAbi('abi-additional/beets/Vault'), ethers.provider);
-    wftm = new ethers.Contract(await vault.WETH(), readAbi('abi-additional/WFTM'), traderAccount);
+    const vault = new ethers.Contract(await pool.getVault(), BeetsVaultAbi, ethers.provider);
+    wftm = new ethers.Contract(await vault.WETH(), WFTMAbi, traderAccount);
 
     await wftm.deposit({
       value: ethers.utils.parseUnits('1', 18)
@@ -165,3 +159,158 @@ describe('BFarmBeets4', function () {
     ]);
   });
 });
+
+const BeetsWeightedPoolAbi = [
+	"constructor(address vault, string name, string symbol, address[] tokens, uint256[] normalizedWeights, uint256 swapFeePercentage, uint256 pauseWindowDuration, uint256 bufferPeriodDuration, address owner)",
+	"event Approval(address indexed owner, address indexed spender, uint256 value)",
+	"event PausedStateChanged(bool paused)",
+	"event SwapFeePercentageChanged(uint256 swapFeePercentage)",
+	"event Transfer(address indexed from, address indexed to, uint256 value)",
+	"function DOMAIN_SEPARATOR() view returns (bytes32)",
+	"function allowance(address owner, address spender) view returns (uint256)",
+	"function approve(address spender, uint256 amount) returns (bool)",
+	"function balanceOf(address account) view returns (uint256)",
+	"function decimals() pure returns (uint8)",
+	"function decreaseApproval(address spender, uint256 amount) returns (bool)",
+	"function getActionId(bytes4 selector) view returns (bytes32)",
+	"function getAuthorizer() view returns (address)",
+	"function getInvariant() view returns (uint256)",
+	"function getLastInvariant() view returns (uint256)",
+	"function getNormalizedWeights() view returns (uint256[])",
+	"function getOwner() view returns (address)",
+	"function getPausedState() view returns (bool paused, uint256 pauseWindowEndTime, uint256 bufferPeriodEndTime)",
+	"function getPoolId() view returns (bytes32)",
+	"function getRate() view returns (uint256)",
+	"function getSwapFeePercentage() view returns (uint256)",
+	"function getVault() view returns (address)",
+	"function increaseApproval(address spender, uint256 amount) returns (bool)",
+	"function name() view returns (string)",
+	"function nonces(address owner) view returns (uint256)",
+	"function onExitPool(bytes32 poolId, address sender, address recipient, uint256[] balances, uint256 lastChangeBlock, uint256 protocolSwapFeePercentage, bytes userData) returns (uint256[], uint256[])",
+	"function onJoinPool(bytes32 poolId, address sender, address recipient, uint256[] balances, uint256 lastChangeBlock, uint256 protocolSwapFeePercentage, bytes userData) returns (uint256[], uint256[])",
+	"function onSwap(tuple(uint8 kind, address tokenIn, address tokenOut, uint256 amount, bytes32 poolId, uint256 lastChangeBlock, address from, address to, bytes userData) request, uint256 balanceTokenIn, uint256 balanceTokenOut) view returns (uint256)",
+	"function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)",
+	"function queryExit(bytes32 poolId, address sender, address recipient, uint256[] balances, uint256 lastChangeBlock, uint256 protocolSwapFeePercentage, bytes userData) returns (uint256 bptIn, uint256[] amountsOut)",
+	"function queryJoin(bytes32 poolId, address sender, address recipient, uint256[] balances, uint256 lastChangeBlock, uint256 protocolSwapFeePercentage, bytes userData) returns (uint256 bptOut, uint256[] amountsIn)",
+	"function setPaused(bool paused)",
+	"function setSwapFeePercentage(uint256 swapFeePercentage)",
+	"function symbol() view returns (string)",
+	"function totalSupply() view returns (uint256)",
+	"function transfer(address recipient, uint256 amount) returns (bool)",
+	"function transferFrom(address sender, address recipient, uint256 amount) returns (bool)"
+];
+
+const BeetsVaultAbi = [
+	"constructor(address authorizer, address weth, uint256 pauseWindowDuration, uint256 bufferPeriodDuration)",
+	"event AuthorizerChanged(address indexed newAuthorizer)",
+	"event ExternalBalanceTransfer(address indexed token, address indexed sender, address recipient, uint256 amount)",
+	"event FlashLoan(address indexed recipient, address indexed token, uint256 amount, uint256 feeAmount)",
+	"event InternalBalanceChanged(address indexed user, address indexed token, int256 delta)",
+	"event PausedStateChanged(bool paused)",
+	"event PoolBalanceChanged(bytes32 indexed poolId, address indexed liquidityProvider, address[] tokens, int256[] deltas, uint256[] protocolFeeAmounts)",
+	"event PoolBalanceManaged(bytes32 indexed poolId, address indexed assetManager, address indexed token, int256 cashDelta, int256 managedDelta)",
+	"event PoolRegistered(bytes32 indexed poolId, address indexed poolAddress, uint8 specialization)",
+	"event RelayerApprovalChanged(address indexed relayer, address indexed sender, bool approved)",
+	"event Swap(bytes32 indexed poolId, address indexed tokenIn, address indexed tokenOut, uint256 amountIn, uint256 amountOut)",
+	"event TokensDeregistered(bytes32 indexed poolId, address[] tokens)",
+	"event TokensRegistered(bytes32 indexed poolId, address[] tokens, address[] assetManagers)",
+	"function WETH() view returns (address)",
+	"function batchSwap(uint8 kind, tuple(bytes32 poolId, uint256 assetInIndex, uint256 assetOutIndex, uint256 amount, bytes userData)[] swaps, address[] assets, tuple(address sender, bool fromInternalBalance, address recipient, bool toInternalBalance) funds, int256[] limits, uint256 deadline) payable returns (int256[] assetDeltas)",
+	"function deregisterTokens(bytes32 poolId, address[] tokens)",
+	"function exitPool(bytes32 poolId, address sender, address recipient, tuple(address[] assets, uint256[] minAmountsOut, bytes userData, bool toInternalBalance) request)",
+	"function flashLoan(address recipient, address[] tokens, uint256[] amounts, bytes userData)",
+	"function getActionId(bytes4 selector) view returns (bytes32)",
+	"function getAuthorizer() view returns (address)",
+	"function getDomainSeparator() view returns (bytes32)",
+	"function getInternalBalance(address user, address[] tokens) view returns (uint256[] balances)",
+	"function getNextNonce(address user) view returns (uint256)",
+	"function getPausedState() view returns (bool paused, uint256 pauseWindowEndTime, uint256 bufferPeriodEndTime)",
+	"function getPool(bytes32 poolId) view returns (address, uint8)",
+	"function getPoolTokenInfo(bytes32 poolId, address token) view returns (uint256 cash, uint256 managed, uint256 lastChangeBlock, address assetManager)",
+	"function getPoolTokens(bytes32 poolId) view returns (address[] tokens, uint256[] balances, uint256 lastChangeBlock)",
+	"function getProtocolFeesCollector() view returns (address)",
+	"function hasApprovedRelayer(address user, address relayer) view returns (bool)",
+	"function joinPool(bytes32 poolId, address sender, address recipient, tuple(address[] assets, uint256[] maxAmountsIn, bytes userData, bool fromInternalBalance) request) payable",
+	"function managePoolBalance(tuple(uint8 kind, bytes32 poolId, address token, uint256 amount)[] ops)",
+	"function manageUserBalance(tuple(uint8 kind, address asset, uint256 amount, address sender, address recipient)[] ops) payable",
+	"function queryBatchSwap(uint8 kind, tuple(bytes32 poolId, uint256 assetInIndex, uint256 assetOutIndex, uint256 amount, bytes userData)[] swaps, address[] assets, tuple(address sender, bool fromInternalBalance, address recipient, bool toInternalBalance) funds) returns (int256[])",
+	"function registerPool(uint8 specialization) returns (bytes32)",
+	"function registerTokens(bytes32 poolId, address[] tokens, address[] assetManagers)",
+	"function setAuthorizer(address newAuthorizer)",
+	"function setPaused(bool paused)",
+	"function setRelayerApproval(address sender, address relayer, bool approved)",
+	"function swap(tuple(bytes32 poolId, uint8 kind, address assetIn, address assetOut, uint256 amount, bytes userData) singleSwap, tuple(address sender, bool fromInternalBalance, address recipient, bool toInternalBalance) funds, uint256 limit, uint256 deadline) payable returns (uint256 amountCalculated)"
+];
+
+const WFTMAbi = [
+	"constructor()",
+	"event Approval(address indexed owner, address indexed spender, uint256 value)",
+	"event Paused(address account)",
+	"event PauserAdded(address indexed account)",
+	"event PauserRemoved(address indexed account)",
+	"event Transfer(address indexed from, address indexed to, uint256 value)",
+	"event Unpaused(address account)",
+	"function ERR_INVALID_ZERO_VALUE() view returns (uint256)",
+	"function ERR_NO_ERROR() view returns (uint256)",
+	"function addPauser(address account)",
+	"function allowance(address owner, address spender) view returns (uint256)",
+	"function approve(address spender, uint256 value) returns (bool)",
+	"function balanceOf(address account) view returns (uint256)",
+	"function decimals() view returns (uint8)",
+	"function decreaseAllowance(address spender, uint256 subtractedValue) returns (bool)",
+	"function deposit() payable returns (uint256)",
+	"function increaseAllowance(address spender, uint256 addedValue) returns (bool)",
+	"function isPauser(address account) view returns (bool)",
+	"function name() view returns (string)",
+	"function pause()",
+	"function paused() view returns (bool)",
+	"function renouncePauser()",
+	"function symbol() view returns (string)",
+	"function totalSupply() view returns (uint256)",
+	"function transfer(address to, uint256 value) returns (bool)",
+	"function transferFrom(address from, address to, uint256 value) returns (bool)",
+	"function unpause()",
+	"function withdraw(uint256 amount) returns (uint256)"
+];
+
+const BeetsBeethovenxMasterChefAbi = [
+	"constructor(address _beets, address _treasuryAddress, uint256 _beetsPerBlock, uint256 _startBlock)",
+	"event Deposit(address indexed user, uint256 indexed pid, uint256 amount, address indexed to)",
+	"event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to)",
+	"event Harvest(address indexed user, uint256 indexed pid, uint256 amount)",
+	"event LogPoolAddition(uint256 indexed pid, uint256 allocPoint, address indexed lpToken, address indexed rewarder)",
+	"event LogSetPool(uint256 indexed pid, uint256 allocPoint, address indexed rewarder, bool overwrite)",
+	"event LogUpdatePool(uint256 indexed pid, uint256 lastRewardBlock, uint256 lpSupply, uint256 accBeetsPerShare)",
+	"event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
+	"event SetTreasuryAddress(address indexed oldAddress, address indexed newAddress)",
+	"event UpdateEmissionRate(address indexed user, uint256 _beetsPerSec)",
+	"event Withdraw(address indexed user, uint256 indexed pid, uint256 amount, address indexed to)",
+	"function POOL_PERCENTAGE() view returns (uint256)",
+	"function TREASURY_PERCENTAGE() view returns (uint256)",
+	"function add(uint256 _allocPoint, address _lpToken, address _rewarder)",
+	"function beets() view returns (address)",
+	"function beetsPerBlock() view returns (uint256)",
+	"function deposit(uint256 _pid, uint256 _amount, address _to)",
+	"function emergencyWithdraw(uint256 _pid, address _to)",
+	"function harvest(uint256 _pid, address _to)",
+	"function harvestAll(uint256[] _pids, address _to)",
+	"function lpTokens(uint256) view returns (address)",
+	"function massUpdatePools(uint256[] pids)",
+	"function owner() view returns (address)",
+	"function pendingBeets(uint256 _pid, address _user) view returns (uint256 pending)",
+	"function poolInfo(uint256) view returns (uint256 allocPoint, uint256 lastRewardBlock, uint256 accBeetsPerShare)",
+	"function poolLength() view returns (uint256)",
+	"function renounceOwnership()",
+	"function rewarder(uint256) view returns (address)",
+	"function set(uint256 _pid, uint256 _allocPoint, address _rewarder, bool overwrite)",
+	"function startBlock() view returns (uint256)",
+	"function totalAllocPoint() view returns (uint256)",
+	"function transferOwnership(address newOwner)",
+	"function treasury(address _treasuryAddress)",
+	"function treasuryAddress() view returns (address)",
+	"function updateEmissionRate(uint256 _beetsPerBlock)",
+	"function updatePool(uint256 _pid) returns (tuple(uint256 allocPoint, uint256 lastRewardBlock, uint256 accBeetsPerShare) pool)",
+	"function userInfo(uint256, address) view returns (uint256 amount, uint256 rewardDebt)",
+	"function withdrawAndHarvest(uint256 _pid, uint256 _amount, address _to)"
+];
+
--- test/BFarmEllipsisTest.js
+++ test/BFarmEllipsisTest.js
@@ -1,14 +1,87 @@
 const chai = require('chai');
-const fs = require('fs');
 const { solidity } = require('ethereum-waffle');
 const { ethers } = require('hardhat');
 
 chai.use(solidity);
 const expect = chai.expect;
 
-function readAbi(name) {
-  return JSON.parse(fs.readFileSync(__dirname + '/../../' + name + '.json').toString());
-}
+const EllipsisStableSwapAbi = [
+  'event TokenExchange(address indexed buyer, int128 sold_id, uint256 tokens_sold, int128 bought_id, uint256 tokens_bought)',
+  'event TokenExchangeUnderlying(address indexed buyer, int128 sold_id, uint256 tokens_sold, int128 bought_id, uint256 tokens_bought)',
+  'event AddLiquidity(address indexed provider, uint256[2] token_amounts, uint256[2] fees, uint256 invariant, uint256 token_supply)',
+  'event RemoveLiquidity(address indexed provider, uint256[2] token_amounts, uint256[2] fees, uint256 token_supply)',
+  'event RemoveLiquidityOne(address indexed provider, uint256 token_amount, uint256 coin_amount, uint256 token_supply)',
+  'event RemoveLiquidityImbalance(address indexed provider, uint256[2] token_amounts, uint256[2] fees, uint256 invariant, uint256 token_supply)',
+  'event RampA(uint256 old_A, uint256 new_A, uint256 initial_time, uint256 future_time)',
+  'event StopRampA(uint256 A, uint256 t)',
+  'constructor()',
+  'function initialize(address _lp_token, address _coin, uint256 _rate_multiplier, uint256 _A, uint256 _fee) @298728',
+  'function admin_fee() view returns (uint256) @456',
+  'function A() view returns (uint256) @10555',
+  'function A_precise() view returns (uint256) @10517',
+  'function get_virtual_price() view returns (uint256) @1015326',
+  'function calc_token_amount(uint256[2] _amounts, bool _is_deposit) view returns (uint256) @2009571',
+  'function add_liquidity(uint256[2] _amounts, uint256 _min_mint_amount) returns (uint256) @3168321',
+  'function add_liquidity(uint256[2] _amounts, uint256 _min_mint_amount, address _receiver) returns (uint256) @3168321',
+  'function get_dy(int128 i, int128 j, uint256 dx) view returns (uint256) @1437364',
+  'function get_dy_underlying(int128 i, int128 j, uint256 dx) view returns (uint256) @1445899',
+  'function exchange(int128 i, int128 j, uint256 _dx, uint256 _min_dy) returns (uint256) @1593683',
+  'function exchange(int128 i, int128 j, uint256 _dx, uint256 _min_dy, address _receiver) returns (uint256) @1593683',
+  'function exchange_underlying(int128 i, int128 j, uint256 _dx, uint256 _min_dy) returns (uint256) @1616338',
+  'function exchange_underlying(int128 i, int128 j, uint256 _dx, uint256 _min_dy, address _receiver) returns (uint256) @1616338',
+  'function remove_liquidity(uint256 _burn_amount, uint256[2] _min_amounts) returns (uint256[2]) @170828',
+  'function remove_liquidity(uint256 _burn_amount, uint256[2] _min_amounts, address _receiver) returns (uint256[2]) @170828',
+  'function remove_liquidity_imbalance(uint256[2] _amounts, uint256 _max_burn_amount) returns (uint256) @3168491',
+  'function remove_liquidity_imbalance(uint256[2] _amounts, uint256 _max_burn_amount, address _receiver) returns (uint256) @3168491',
+  'function calc_withdraw_one_coin(uint256 _burn_amount, int128 i) view returns (uint256) @1201',
+  'function remove_liquidity_one_coin(uint256 _burn_amount, int128 i, uint256 _min_received) returns (uint256) @1987275',
+  'function remove_liquidity_one_coin(uint256 _burn_amount, int128 i, uint256 _min_received, address _receiver) returns (uint256) @1987275',
+  'function ramp_A(uint256 _future_A, uint256 _future_time) @161397',
+  'function stop_ramp_A() @157438',
+  'function admin_balances(uint256 i) view returns (uint256) @7834',
+  'function withdraw_admin_fees() @38799',
+  'function factory() view returns (address) @3096',
+  'function lp_token() view returns (address) @3126',
+  'function coins(uint256 arg0) view returns (address) @3201',
+  'function balances(uint256 arg0) view returns (uint256) @3231',
+  'function fee() view returns (uint256) @3216',
+  'function initial_A() view returns (uint256) @3246',
+  'function future_A() view returns (uint256) @3276',
+  'function initial_A_time() view returns (uint256) @3306',
+  'function future_A_time() view returns (uint256) @3336'
+];
+
+const EllipsisLpStakingAbi = [
+  'constructor(address _rewardToken, address _incentiveVoting, address _tokenLocker, uint256 _maxMintable)',
+  'event ClaimedReward(address indexed caller, address indexed claimer, address indexed receiver, uint256 amount)',
+  'event Deposit(address indexed user, address indexed token, uint256 amount)',
+  'event EmergencyWithdraw(address indexed token, address indexed user, uint256 amount)',
+  'event FeeClaimRevert(address pool)',
+  'event FeeClaimSuccess(address pool)',
+  'event Withdraw(address indexed user, address indexed token, uint256 amount)',
+  'function addPool(address _token) returns (bool)',
+  'function blockThirdPartyActions(address) view returns (bool)',
+  'function claim(address _user, address[] _tokens) returns (uint256)',
+  'function claimReceiver(address) view returns (address)',
+  'function claimableReward(address _user, address[] _tokens) view returns (uint256[])',
+  'function deposit(address _token, uint256 _amount, bool _claimRewards) returns (uint256)',
+  'function emergencyWithdraw(address _token)',
+  'function incentiveVoting() view returns (address)',
+  'function lastFeeClaim(address) view returns (uint256)',
+  'function maxMintableTokens() view returns (uint256)',
+  'function mintedTokens() view returns (uint256)',
+  'function poolInfo(address) view returns (uint256 adjustedSupply, uint256 rewardsPerSecond, uint256 lastRewardTime, uint256 accRewardPerShare)',
+  'function poolLength() view returns (uint256)',
+  'function registeredTokens(uint256) view returns (address)',
+  'function rewardToken() view returns (address)',
+  'function setBlockThirdPartyActions(bool _block)',
+  'function setClaimReceiver(address _receiver)',
+  'function startTime() view returns (uint256)',
+  'function tokenLocker() view returns (address)',
+  'function updateUserBoosts(address _user, address[] _tokens)',
+  'function userInfo(address, address) view returns (uint256 depositAmount, uint256 adjustedAmount, uint256 rewardDebt, uint256 claimable)',
+  'function withdraw(address _token, uint256 _amount, bool _claimRewards) returns (uint256)'
+];
 
 describe('BFarmEllipsis', function () {
   let bFarmEllipsis, pool, ellipsisLpStaking, rewardTokenContract;
@@ -43,10 +116,10 @@ describe('BFarmEllipsis', function () {
     }
 
     const poolAddress = await bFarmEllipsis.pool();
-    pool = new ethers.Contract(poolAddress, readAbi('abi-additional/ellipsis/StableSwap'), ethers.provider);
+    pool = new ethers.Contract(poolAddress, EllipsisStableSwapAbi, ethers.provider);
 
     const ellipsisLpStakingAddress = await bFarmEllipsis.ellipsisLpStaking();
-    ellipsisLpStaking = new ethers.Contract(ellipsisLpStakingAddress, readAbi('abi-additional/ellipsis/EllipsisLpStaking'), ethers.provider);
+    ellipsisLpStaking = new ethers.Contract(ellipsisLpStakingAddress, EllipsisLpStakingAbi, ethers.provider);
 
     const rewardTokenAddress = await ellipsisLpStaking.rewardToken();
     rewardTokenContract = await ethers.getContractAt('ERC20', rewardTokenAddress);
